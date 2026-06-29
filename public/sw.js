1f1748ac26408c945903b5a2563da84bb11e29e5
Jan <janperezgonzalez@gmail.com>
2026-06-29 16:43:52 +0200

    Fix image analysis: restore FileReader with HEIC fallback, stop caching index.html in SW

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'wellness-v3';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'wellness-v4';
+const ASSETS = ['/manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
